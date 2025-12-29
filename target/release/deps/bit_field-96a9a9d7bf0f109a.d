dcd30fb515a13295765b19bd317e2fa042799c50
Preon1 <preon1@gmail.com>
2025-12-29 17:15:51 +0100

    cleanup

--- target/release/deps/bit_field-96a9a9d7bf0f109a.d
+++
@@ -1,7 +0,0 @@
-\\nas\Home\exchange\rust_perf_test\target\release\deps\bit_field-96a9a9d7bf0f109a.d: C:\Users\blackbox\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\bit_field-0.10.3\src\lib.rs
-
-\\nas\Home\exchange\rust_perf_test\target\release\deps\libbit_field-96a9a9d7bf0f109a.rlib: C:\Users\blackbox\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\bit_field-0.10.3\src\lib.rs
-
-\\nas\Home\exchange\rust_perf_test\target\release\deps\libbit_field-96a9a9d7bf0f109a.rmeta: C:\Users\blackbox\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\bit_field-0.10.3\src\lib.rs
-
-C:\Users\blackbox\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\bit_field-0.10.3\src\lib.rs:
--- target/release/rust_perf_test.d
+++
@@ -1,1 +0,0 @@
-/Users/serge/code/rust_perf_test/target/release/rust_perf_test: /Users/serge/code/rust_perf_test/src/main.rs
